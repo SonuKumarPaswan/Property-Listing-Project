67b5783f61d4d769a32605e3ba08ce2d44cd051b
Akanshu Dutt <akanshudutt@gmail.com>
2026-03-31 12:16:36 +0530

    fix: ignore eslint build errors

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.29.7"],
-
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
