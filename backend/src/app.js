f4036a5f37250bbbd261fc9aedc41f55541c562f
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2026-04-17 14:47:47 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://mishti-houses-frontend.onrender.com"
+        "https://mishti-houses.vercel.app/"
      ],
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
