6fab521c7e12967bbc378b353acc70ea467c4419
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2026-04-17 15:44:35 +0530

    add api

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://mishti-houses.vercel.app/"
+        "https://mishti-houses.vercel.app"
      ],
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
